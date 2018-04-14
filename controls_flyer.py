7fc58fec36c23807b9598f16463f332900154f3b
Alexey <alexey.simonov@gmail.com>
2018-04-14 14:27:03 +0100

    refactoring

--- controls_flyer.py
+++ controls_flyer.py
@@ -30,7 +30,6 @@ class ControlsFlyer(UnityDrone):
 
     def __init__(self, connection):
         super().__init__(connection)
-        self.controller = NonlinearController()
         self.target_position = np.array([0.0, 0.0, 0.0])
         self.all_waypoints = []
         self.in_mission = True
@@ -39,93 +38,14 @@ class ControlsFlyer(UnityDrone):
         # initial state
         self.flight_state = States.MANUAL
 
+        self.controller = NonlinearController()
+
         # register all your callbacks here
-        self.register_callback(MsgID.LOCAL_POSITION,
-                               self.local_position_callback)
+        self.register_callback(MsgID.STATE,          self.state_callback)
+        self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
         self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
-        self.register_callback(MsgID.STATE, self.state_callback)
-        
-        self.register_callback(MsgID.ATTITUDE, self.attitude_callback)
-        self.register_callback(MsgID.RAW_GYROSCOPE, self.gyro_callback)
-        
-    def position_controller(self):  
-        (self.local_position_target,
-         self.local_velocity_target,
-         yaw_cmd) = self.controller.trajectory_control(
-                 self.position_trajectory,
-                 self.yaw_trajectory,
-                 self.time_trajectory, time.time())
-        self.attitude_target = np.array((0.0, 0.0, yaw_cmd))
-        acceleration_cmd = self.controller.lateral_position_control(
-                self.local_position_target[0:2],
-                self.local_velocity_target[0:2],
-                self.local_position[0:2],
-                self.local_velocity[0:2])
-        self.local_acceleration_target = np.array([acceleration_cmd[0],
-                                                   acceleration_cmd[1],
-                                                   0.0])
-        
-    def attitude_controller(self):
-        self.thrust_cmd = self.controller.altitude_control(
-                -self.local_position_target[2],
-                -self.local_velocity_target[2],
-                -self.local_position[2],
-                -self.local_velocity[2],
-                self.attitude,
-                9.81)
-        roll_pitch_rate_cmd = self.controller.roll_pitch_controller(
-                self.local_acceleration_target[0:2],
-                self.attitude,
-                self.thrust_cmd)
-        yawrate_cmd = self.controller.yaw_control(
-                self.attitude_target[2],
-                self.attitude[2])
-        self.body_rate_target = np.array(
-                [roll_pitch_rate_cmd[0], roll_pitch_rate_cmd[1], yawrate_cmd])
-        
-    def bodyrate_controller(self):        
-        moment_cmd = self.controller.body_rate_control(
-                self.body_rate_target,
-                self.gyro_raw)
-        self.cmd_moment(moment_cmd[0],
-                        moment_cmd[1],
-                        moment_cmd[2],
-                        self.thrust_cmd)
-    
-    def attitude_callback(self):
-        if self.flight_state == States.WAYPOINT:
-            self.attitude_controller()
-    
-    def gyro_callback(self):
-        if self.flight_state == States.WAYPOINT:
-            self.bodyrate_controller()
-
-    def local_position_callback(self):
-        if self.flight_state == States.TAKEOFF:
-            if -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
-                #self.all_waypoints = self.calculate_box()
-                (self.position_trajectory,
-                 self.time_trajectory,
-                 self.yaw_trajectory) = self.load_test_trajectory(time_mult=0.5)
-                self.all_waypoints = self.position_trajectory.copy()
-                self.waypoint_number = -1
-                self.waypoint_transition()
-        elif self.flight_state == States.WAYPOINT:
-
-            if time.time() > self.time_trajectory[self.waypoint_number]:
-                if len(self.all_waypoints) > 0:
-                    self.waypoint_transition()
-                else:
-                    if np.linalg.norm(self.local_velocity[0:2]) < 1.0:
-                        self.landing_transition()
-
-    def velocity_callback(self):
-        if self.flight_state == States.LANDING:
-            if self.global_position[2] - self.global_home[2] < 0.1:
-                if abs(self.local_position[2]) < 0.01:
-                    self.disarming_transition()
-        if self.flight_state == States.WAYPOINT:
-            self.position_controller()
+        self.register_callback(MsgID.ATTITUDE,       self.attitude_callback)
+        self.register_callback(MsgID.RAW_GYROSCOPE,  self.gyro_callback)
 
     def state_callback(self):
         if self.in_mission:
@@ -138,13 +58,91 @@ class ControlsFlyer(UnityDrone):
                 if ~self.armed & ~self.guided:
                     self.manual_transition()
 
-    def calculate_box(self):
-        print("Setting Home")
-        local_waypoints = [[10.0, 0.0, -3.0],
-                           [10.0, 10.0, -3.0],
-                           [0.0, 10.0, -3.0],
-                           [0.0, 0.0, -3.0]]
-        return local_waypoints
+    def local_position_callback(self):
+        if self.flight_state == States.TAKEOFF:
+            if -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
+                # PLAN PATH
+                #self.all_waypoints = self.calculate_box()
+                (self.position_trajectory,
+                 self.time_trajectory,
+                 self.yaw_trajectory) = self.load_test_trajectory(time_mult=0.5)
+                self.all_waypoints = self.position_trajectory.copy()
+                self.waypoint_number = -1
+                # EXECUTE PATH: start
+                self.waypoint_transition()
+        elif self.flight_state == States.WAYPOINT:
+            if time.time() > self.time_trajectory[self.waypoint_number]:
+                if len(self.all_waypoints) > 0:
+                    # EXECUTE PATH: continue
+                    self.waypoint_transition()
+                else:
+                    if np.linalg.norm(self.local_velocity[0:2]) < 1.0:
+                        # EXECUTE PATH: finish
+                        self.landing_transition()
+
+    def attitude_callback(self):
+        if self.flight_state == States.WAYPOINT:
+            #self.attitude_controller()
+            self.thrust_cmd = self.controller.altitude_control(
+                -self.local_position_target[2],
+                -self.local_velocity_target[2],
+                -self.local_position[2],
+                -self.local_velocity[2],
+                self.attitude,
+                9.81)
+            roll_pitch_rate_cmd = self.controller.roll_pitch_controller(
+                self.local_acceleration_target[0:2],
+                self.attitude,
+                self.thrust_cmd)
+            yawrate_cmd = self.controller.yaw_control(
+                self.attitude_target[2],
+                self.attitude[2])
+            self.body_rate_target = np.array(
+                [roll_pitch_rate_cmd[0], roll_pitch_rate_cmd[1], yawrate_cmd])
+
+    def gyro_callback(self):
+        if self.flight_state == States.WAYPOINT:
+            #self.bodyrate_controller()
+            moment_cmd = self.controller.body_rate_control(
+                self.body_rate_target,
+                self.gyro_raw)
+            self.cmd_moment(moment_cmd[0],
+                            moment_cmd[1],
+                            moment_cmd[2],
+                            self.thrust_cmd)
+
+    def velocity_callback(self):
+        if self.flight_state == States.LANDING:
+            # landed? disarm
+            if self.global_position[2] - self.global_home[2] < 0.1:
+                if abs(self.local_position[2]) < 0.01:
+                    self.disarming_transition()
+        if self.flight_state == States.WAYPOINT:
+            # in mission? control position and speed
+            #self.position_controller()
+            (self.local_position_target,
+             self.local_velocity_target,
+             yaw_cmd) = self.controller.trajectory_control(
+                     self.position_trajectory,
+                     self.yaw_trajectory,
+                     self.time_trajectory, time.time())
+            self.attitude_target = np.array((0.0, 0.0, yaw_cmd))
+            acceleration_cmd = self.controller.lateral_position_control(
+                    self.local_position_target[0:2],
+                    self.local_velocity_target[0:2],
+                    self.local_position[0:2],
+                    self.local_velocity[0:2])
+            self.local_acceleration_target = np.array([acceleration_cmd[0],
+                                                       acceleration_cmd[1],
+                                                       0.0])
+
+#    def calculate_box(self):
+#        print("Setting Home")
+#        local_waypoints = [[10.0, 0.0, -3.0],
+#                           [10.0, 10.0, -3.0],
+#                           [0.0, 10.0, -3.0],
+#                           [0.0, 0.0, -3.0]]
+#        return local_waypoints
 
     def arming_transition(self):
         print("arming transition")
